f7aa855b84be4d3ee31f5dac8ede8adee1c227ee
Jason Cheah <cheah.annshen@gmail.com>
2018-11-28 23:27:47 +0000

    banner commit

--- js/myscripts.js
+++ js/myscripts.js
@@ -5,3 +5,56 @@ window.onload = function () {
 function init() {
   console.log("Loaded");
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+autoscroll();
+
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n)
+{
+
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+
+    if (n > x.length)
+    {
+      slideIndex = 1;
+    }
+
+    if (n < 1)
+    {
+      slideIndex = x.length;
+    }
+
+    for (i = 0; i < x.length; i++)
+    {
+      x[i].style.display = "none";
+    }
+
+    x[slideIndex - 1].style.display = "block";
+}
+
+function autoscroll()
+{
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+
+  for(i = 0; i < x.length; i++)
+  {
+    x[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex >  x.length)
+  {
+    slideIndex = 1;
+  }
+
+  x[slideIndex - 1].style.display = "block"
+  setTimeout(autoscroll, 3000);
+}
